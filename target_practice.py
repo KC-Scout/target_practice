169bee43342c6b9f9d4940a6c807accc37e38bd2
Connor Foster <foster.clee@gmail.com>
2019-03-27 22:04:43 -0500

    make background screen green, load ship, draw ship on mid left of screen

--- target_practice.py
+++ target_practice.py
@@ -1,1 +1,27 @@
 import pygame
+import pygame.sprite
+import sys
+
+screen = pygame.display.set_mode((1200, 800))
+screen_rect = screen.get_rect()
+
+image = pygame.image.load('ship.bmp')
+image_rect = image.get_rect()
+image_rect.midleft = screen_rect.midleft
+
+while True:
+    
+    screen.fill((0, 255, 0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                sys.exit()
+        elif event.type == pygame.QUIT:
+            sys.exit()
+    
+ 
+    
+    
+    screen.blit(image, image_rect)
+    pygame.display.flip()
