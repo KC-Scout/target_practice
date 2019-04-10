2f0938391150fa07675ca9d19bbe7efe9312a74b
Connor Foster <foster.clee@gmail.com>
2019-04-10 07:58:26 -0500

    image moves seamlessly up and down screen via flags

--- target_practice.py
+++ target_practice.py
@@ -10,8 +10,8 @@ screen_rect = screen.get_rect()
 image = pygame.image.load('ship.bmp')
 image_rect = image.get_rect()
 image_rect.midleft = screen_rect.midleft
-image_rect_flag = False
-
+image_move_down = False
+image_move_up = False
 
 target_rect = pygame.Rect(400, 0, 100, 100)
 target_color = (255, 0, 0)
@@ -32,19 +32,32 @@ while True:
             if event.key == pygame.K_q:
                 sys.exit()
                 
-            # Move the ship up and down 
+            # Move flag ships movement up and down 
             elif event.key == pygame.K_UP:
-                image_rect.y -= 45
+                image_move_up = True
             elif event.key == pygame.K_DOWN:
-                image_rect.y += 45                
+                image_move_down = True
+                
             # Active bullet fired 
             elif event.key == pygame.K_SPACE:
                 bullet_rect.midright = image_rect.midright
                 fire_bullet = True
-                
+        
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                image_move_up = False
+            if event.key == pygame.K_DOWN:
+                image_move_down = False
+        
         elif event.type == pygame.QUIT:
                 sys.exit()
                 
+    # Move the image up and down
+    if image_move_up and image_rect.top > screen_rect.top:
+        image_rect.y -= 2
+    elif image_move_down and image_rect.bottom < screen_rect.bottom:
+        image_rect.y += 2
+            
     # Move the bullet across the screen if fired
     if fire_bullet:
         screen.fill(bullet_color, bullet_rect)
